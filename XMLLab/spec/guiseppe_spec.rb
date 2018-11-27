467fa846a2ba10f4c7bde21d1564bd3cf455d880
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-11-27 10:59:07 +0000

    V.0.0.2 Created my first test, testing 'should have no prices more than £10'

--- XMLLab/spec/guiseppe_spec.rb
+++ XMLLab/spec/guiseppe_spec.rb
@@ -1,23 +1,10 @@
-require 'spec_helper'
+describe 'Guiseppes'do
 
-describe "XML menu" do
-
-  before(:all) do
-    @xml_menu = GuiseppesMenu.new
+  before(:all)do
+    @menu = Guiseppes.new
   end
 
-  it "no price should be more than £10" do
-    pending
+  it 'should have no price more than £10' do
+    
   end
-
-  it "should have no item with calories over 1000 except for the full breakfast" do
-    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
-    pending
-  end
-
-  it "should have all waffle dishes stating you get two waffles" do
-    # Consider children element methods - Remember to step through the data and print out to the command line if you need it
-    pending
-  end
-
 end
